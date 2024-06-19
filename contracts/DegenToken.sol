366bf68be30c2d79138188a2bd41ab6a66199502
Jigs Manzano <108137065+JigsManzano12@users.noreply.github.com>
2024-06-19 12:02:01 +0800

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -5,10 +5,30 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
+    
+    constructor() ERC20("Degen", "DGN") {
+        _mint(msg.sender, 1000000 * 10 ** uint(decimals()));
+    }
 
-    constructor() ERC20("Degen", "DGN") {}
+    function checkBalance(address account) public view returns (uint256) {
+        return balanceOf(account);
+    }
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    function burn(uint256 amount) public {
+        _burn(_msgSender(), amount);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), to, amount);
+        return true;
+    }
+
+    function redeem(uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender, amount);
     }
 }
