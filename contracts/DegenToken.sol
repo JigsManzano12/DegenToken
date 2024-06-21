53870f88d4668eaa426b5b032943189b193987ac
Jigs Manzano <108137065+JigsManzano12@users.noreply.github.com>
2024-06-21 21:54:17 +0800

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -5,9 +5,35 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
-    
+    string[] private swords = [
+        "Longclaw",
+        "Ice",
+        "Oathkeeper",
+        "Widow's Wail",
+        "Heartsbane",
+        "Dawn",
+        "Blackfyre",
+        "Dark Sister",
+        "Needle",
+        "Lightbringer"
+    ];
+
+    mapping(string => uint256) public swordPrices;
+    mapping(address => string) public redeemedItems;
+
     constructor() ERC20("Degen", "DGN") {
         _mint(msg.sender, 1000000 * 10 ** uint(decimals()));
+
+        swordPrices["Longclaw"] = 100 * 10 ** uint(decimals());
+        swordPrices["Ice"] = 200 * 10 ** uint(decimals());
+        swordPrices["Oathkeeper"] = 150 * 10 ** uint(decimals());
+        swordPrices["Widow's Wail"] = 180 * 10 ** uint(decimals());
+        swordPrices["Heartsbane"] = 130 * 10 ** uint(decimals());
+        swordPrices["Dawn"] = 210 * 10 ** uint(decimals());
+        swordPrices["Blackfyre"] = 170 * 10 ** uint(decimals());
+        swordPrices["Dark Sister"] = 160 * 10 ** uint(decimals());
+        swordPrices["Needle"] = 90 * 10 ** uint(decimals());
+        swordPrices["Lightbringer"] = 220 * 10 ** uint(decimals());
     }
 
     function checkBalance(address account) public view returns (uint256) {
@@ -29,6 +55,19 @@ contract DegenToken is ERC20, Ownable {
 
     function redeem(uint256 amount) external {
         require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-        _burn(msg.sender, amount);
+
+        uint256 randomIndex = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % swords.length;
+        string memory sword = swords[randomIndex];
+
+        uint256 swordPrice = swordPrices[sword];
+        require(amount >= swordPrice, "Insufficient tokens to redeem this sword");
+
+        _burn(msg.sender, swordPrice);
+
+        redeemedItems[msg.sender] = sword;
+    }
+
+    function getRedeemedItem(address account) external view returns (string memory) {
+        return redeemedItems[account];
     }
 }
